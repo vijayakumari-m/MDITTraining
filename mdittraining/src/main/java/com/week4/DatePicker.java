b9cf5cc8efd749d8449311ff54528d7af75dfa81
Vijayakumari <vijayakumari.m@moolya.com>
2023-04-03 12:47:14 +0530

    Implemented the code review comments

--- mdittraining/src/main/java/com/week4/DatePicker.java
+++ mdittraining/src/main/java/com/week4/DatePicker.java
@@ -1,5 +1,7 @@
 package com.week4;
 
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,6 +12,7 @@ public class DatePicker {
 
 	public static void main(String[] args) {
 
+		//Adding ChromeOptions as Selenium 4.8.1 has some issues in connecting with chrome driver
 		ChromeOptions co = new ChromeOptions();
 		co.addArguments("--remote-allow-origins=*");
 		
@@ -22,16 +25,38 @@ public class DatePicker {
 		driver.manage().window().maximize();
 
 		// inspect the frame and switch to it
-		WebElement demoFrame = driver.findElement(By.className("demo-frame"));
+		WebElement demoFrame = null;
+		WebElement dateField = null;
+		WebElement date = null;
+		
+		//try catch block to handle when the exception occurs
+		try {
+			demoFrame = driver.findElement(By.className("demo-frame"));
+		}
+		catch (NoSuchElementException e) {
+			System.out.println(e);
+		}
 		driver.switchTo().frame(demoFrame);
-
-		WebElement dateField = driver.findElement(By.xpath("//input[@id='datepicker']"));
+		
+		try {
+			dateField= driver.findElement(By.xpath("//input[@id='datepicker']"));
+		}
+		catch (NoSuchElementException e) {
+			System.out.println(e);
+		}
 		dateField.click();
-
-		WebElement date = driver.findElement(By.xpath("//td/a[text()='8']"));
+		
+		try {
+			date= driver.findElement(By.xpath("//td/a[text()='8']"));
+		}
+		catch (NoSuchElementException e) {
+			System.out.println(e);
+		}
 		date.click();
-
-		driver.close();
+		
+		
+		driver.quit();
+		
 
 	}
 
