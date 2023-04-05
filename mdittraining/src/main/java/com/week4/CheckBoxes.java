0ae150bdceac28cf5677286c9a34705a6c398633
Vijayakumari <vijayakumari.m@moolya.com>
2023-04-05 16:17:05 +0530

    Implemented the review comments for CheckBoxes program

--- mdittraining/src/main/java/com/week4/CheckBoxes.java
+++ mdittraining/src/main/java/com/week4/CheckBoxes.java
@@ -1,5 +1,8 @@
 package com.week4;
 
+import java.util.List;
+import java.util.Scanner;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,28 +13,38 @@ public class CheckBoxes {
 
 	public static void main(String[] args) {
 
-
 		ChromeOptions co = new ChromeOptions();
 		co.addArguments("--remote-allow-origins=*");
-		
+
 		// Initialize the driver
 		WebDriver driver = new ChromeDriver(co);
-						
-		//Get URL
+
+		// Get URL
 		driver.get("https://www.ironspider.ca/forms/checkradio.htm");
-						
+
 		driver.manage().window().maximize();
 		
-		WebElement checkbox1 = driver.findElement(By.xpath("//input[@value='red']"));
-		checkbox1.click();
-		
-		WebElement checkbox2 = driver.findElement(By.xpath("//input[@value='blue']"));
-		checkbox2.click();
-		
-		WebElement checkbox3 = driver.findElement(By.xpath("//input[@value='purple']"));
-		checkbox3.click();
-		
-		driver.close();
+		//Pass any three colours to the String variable
+		String colour1 = "red";
+		String colour2 = "blue";
+		String colour3 = "purple";
+
+		//Inspect all the colours checkbox and store it in a list
+		List<WebElement> checkBoxOptions = driver.findElements(By.xpath("//input[@type='checkbox']"));
+
+		//Loop through the list and if the desire variable/colour comes, then select them
+		for (WebElement options : checkBoxOptions) {
+			
+			//Get the value of the attribute "value"
+			String colour = options.getAttribute("value");
+
+			if (colour.equalsIgnoreCase(colour1)||colour.equalsIgnoreCase(colour2)||colour.equalsIgnoreCase(colour3)) {
+
+				options.click();
+			}
+		}
+
+		driver.quit();
 
 	}
 
