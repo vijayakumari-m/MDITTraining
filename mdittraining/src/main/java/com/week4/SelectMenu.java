65aca6e16a7b273ca4c39375013d0b7e7982580b
Vijayakumari <vijayakumari.m@moolya.com>
2023-04-25 17:20:58 +0530

    Implemented the review comments for Program2 and Program4

--- mdittraining/src/main/java/com/week4/SelectMenu.java
+++ mdittraining/src/main/java/com/week4/SelectMenu.java
@@ -1,6 +1,7 @@
 package com.week4;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -31,41 +32,53 @@ public class SelectMenu {
 		//inspect the frame and switch to it
 		WebElement demoFrame = driver.findElement(By.className("demo-frame"));
 		driver.switchTo().frame(demoFrame);
+		
+		WebElement speed = null;
+		WebElement files = null;
+		WebElement titlefield = null;
+		WebElement number = null;
+		
+		try {
+			//Select speed as Medium 
+			speed = driver.findElement(By.id("speed-button"));
+			speed.click();
+		
+			//Implement Actions class to use mouse or keyboard actions
+			Actions act = new Actions(driver);
+			act.click(speed);//click on speed menu from the drop down
+			act.sendKeys(Keys.ARROW_DOWN).build().perform();//press down arrow from the keyboard
+			act.sendKeys(Keys.ARROW_UP).build().perform(); // press down arrow from the keyboard
+				
+			//Select a title 
+			titlefield = driver.findElement(By.id("salutation-button"));
+			titlefield.click();
+				
+				
+			act.click(titlefield);//click on desire title from the drop down
+			act.sendKeys(Keys.ARROW_DOWN).build().perform();//press down arrow from the keyboard
+			act.sendKeys(Keys.ARROW_DOWN).build().perform();//press down arrow from the keyboard
+				
+			//Select files 
+			files = driver.findElement(By.id("files-button"));
+				
+			act.click(files);//click on desire file from the drop down
+			act.sendKeys(Keys.ARROW_DOWN).build().perform();//press down arrow from the keyboard
+				
+			//Select number 
+			number = driver.findElement(By.id("number-button"));
+				
+			act.click(number);//click on desire number from the drop down
+			act.sendKeys(Keys.ARROW_DOWN).build().perform();//press down arrow from the keyboard
+			act.sendKeys(Keys.ARROW_DOWN).build().perform();//press down arrow from the keyboard
+			act.sendKeys(Keys.ARROW_DOWN).build().perform();//press down arrow from the keyboard
+				
+		}
+		
+		
+		catch (NoSuchElementException e) {
 			
-		//Select speed as Medium 
-		WebElement speed = driver.findElement(By.id("speed-button"));
-		speed.click();
-		
-		//Implement Actions class to use mouse or keyboard actions
-		Actions act = new Actions(driver);
-		act.click(speed);
-		act.sendKeys(Keys.ARROW_DOWN).build().perform();
-		act.sendKeys(Keys.ARROW_UP).build().perform();
-
-		
-		
-		//Select a title 
-		WebElement titlefield = driver.findElement(By.id("salutation-button"));
-		titlefield.click();
-		
-		
-		act.click(titlefield);
-		act.sendKeys(Keys.ARROW_DOWN).build().perform();
-		act.sendKeys(Keys.ARROW_DOWN).build().perform();
-		
-		//Select files 
-		WebElement files = driver.findElement(By.id("files-button"));
-		
-		act.click(files);
-		act.sendKeys(Keys.ARROW_DOWN).build().perform();
-		
-		//Select number 
-		WebElement number = driver.findElement(By.id("number-button"));
-		
-		act.click(number);
-		act.sendKeys(Keys.ARROW_DOWN).build().perform();
-		act.sendKeys(Keys.ARROW_DOWN).build().perform();
-		act.sendKeys(Keys.ARROW_DOWN).build().perform();
+			System.out.println(e);
+		}
 		
 		driver.close();
 	
