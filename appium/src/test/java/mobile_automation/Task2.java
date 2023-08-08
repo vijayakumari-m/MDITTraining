e1a747d82a7dffc014e0d106e86d8b322785dd41
Vijayakumari <vijayakumari.m@moolya.com>
2023-08-08 14:47:57 +0530

    Updated Task2

--- appium/src/test/java/mobile_automation/Task2.java
+++ appium/src/test/java/mobile_automation/Task2.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.HttpCommandExecutor;
 import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.Test;
@@ -13,8 +14,10 @@ import org.testng.annotations.Test;
 import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.touch.TapOptions;
 
 public class Task2 {
 	
@@ -40,7 +43,7 @@ public class Task2 {
 		driver.findElement(AppiumBy.accessibilityId("View")).click();
 		driver.findElement(AppiumBy.accessibilityId("Gallery")).click();
 		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"1. Photos\"]")).click();
-		
+		WebElement we =driver.findElement(By.xpath(""));
 		//Swipe Action 
 		RemoteWebElement src = (RemoteWebElement) driver.findElement(By.xpath("android.widget.ImageView[1]"));
 		((JavascriptExecutor)driver).executeScript("mobile:swipeGesture", ImmutableMap.of(
@@ -48,6 +51,12 @@ public class Task2 {
 				"endX", 100,
 				"endY", 200
 				));
+		
+		//tap action
+		@SuppressWarnings({ "rawtypes", "deprecation" })
+		TouchAction t =new TouchAction(driver);
+		t.tap(tapOptions()
+       			.withElement(element(we))).perform();
 		
 		driver.quit();
 		
